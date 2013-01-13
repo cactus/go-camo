c586716bd5f45a9cf72a377b2f9d8e02f29cbef8
elij <elij.mx@gmail.com>
2013-01-13 13:38:50 -0800

    some clean up in the encoding code

--- camoproxy/encoding/url.go
+++ camoproxy/encoding/url.go
@@ -3,6 +3,7 @@ package encoding
 import (
 	"crypto/hmac"
 	"crypto/sha1"
+	"crypto/subtle"
 	"encoding/hex"
 	"github.com/cactus/gologit"
 )
@@ -10,31 +11,36 @@ import (
 // DecodeUrl ensures the url is properly verified via HMAC, and then
 // unencodes the url, returning the url (if valid) and whether the
 // HMAC was verified.
-func DecodeUrl(hmackey *[]byte, hexdig string, hexurl string) (surl string, valid bool) {
+func DecodeUrl(hmackey *[]byte, hexdig string, hexurl string) (string, bool) {
 	urlBytes, err := hex.DecodeString(hexurl)
 	if err != nil {
-		gologit.Debugln("Bad Hex Decode", hexurl)
-		return
+		gologit.Debugln("Bad Hex Decode of URL", hexurl)
+		return "", false
 	}
-	surl = string(urlBytes)
+	macBytes, err := hex.DecodeString(hexdig)
+	if err != nil {
+		gologit.Debugln("Bad Hex Decode of MAC", hexurl)
+		return "", false
+	}
+
 	mac := hmac.New(sha1.New, *hmackey)
-	mac.Write([]byte(surl))
-	macSum := hex.EncodeToString(mac.Sum([]byte{}))
-	if macSum != hexdig {
-		gologit.Debugf("Bad signature: %s != %s\n", macSum, hexdig)
-		return
+	mac.Write(urlBytes)
+	macSum := mac.Sum(nil)
+	if subtle.ConstantTimeCompare(macSum, macBytes) != 1 {
+		gologit.Debugf("Bad signature: %x != %x\n", macSum, macBytes)
+		return "", false
 	}
-	valid = true
-	return
+	return string(urlBytes), true
 }
 
 // EncodeUrl takes an HMAC key and a url, and returns url
 // path partial consisitent of signature and encoded url.
 func EncodeUrl(hmacKey *[]byte, oUrl string) string {
+	oBytes := []byte(oUrl)
 	mac := hmac.New(sha1.New, *hmacKey)
-	mac.Write([]byte(oUrl))
-	macSum := hex.EncodeToString(mac.Sum([]byte{}))
-	encodedUrl := hex.EncodeToString([]byte(oUrl))
+	mac.Write(oBytes)
+	macSum := hex.EncodeToString(mac.Sum(nil))
+	encodedUrl := hex.EncodeToString(oBytes)
 	hexurl := "/" + macSum + "/" + encodedUrl
 	return hexurl
 }
