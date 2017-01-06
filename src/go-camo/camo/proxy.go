a06e1429db18785717e14c927bfd0b74e65d8292
Alex <alex@zeitgeist.se>
2017-01-06 20:43:00 +0100

    Fix memory leak - the client must always close the response body, even when err is different from nil

--- src/go-camo/camo/proxy.go
+++ src/go-camo/camo/proxy.go
@@ -166,6 +166,11 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	mlog.Debugm("built outgoing request", mlog.Map{"req": nreq})
 
 	resp, err := p.client.Do(nreq)
+
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+
 	if err != nil {
 		mlog.Debugm("could not connect to endpoint", mlog.Map{"err": err})
 		// this is a bit janky, but better than peeling off the
@@ -186,7 +191,6 @@ func (p *Proxy) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		}
 		return
 	}
-	defer resp.Body.Close()
 
 	mlog.Debugm("response from upstream", mlog.Map{"resp": resp})
 
