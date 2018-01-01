ec725a7df6f9ffed731f0e9a9c8a6bed40fe1729
elij <elij@wundrd.net>
2017-12-31 23:01:42 -0800

    update dependencies to current versions

--- src/go-camo/version_info_generated.go
+++ src/go-camo/version_info_generated.go
@@ -11,9 +11,14 @@ This software is available under the MIT License at:
 └── https://github.com/cactus/go-camo
 
 Portions of this software utilize third party libraries:
-├── https://github.com/cactus/mlog              (MIT License)
-├── https://github.com/jessevdk/go-flags        (BSD License)
-└── github.com/stretchr/testify/assert          (MIT License)
-    ├── https://github.com/davecgh/go-spew      (ISC License)
-    └── https://github.com/pmezard/go-difflib   (BSD License)
+
+*   Runtime dependencies:
+    ├── https://github.com/cactus/mlog              (MIT License)
+    ├── https://github.com/cactus/tai64             (MIT License)
+    └── https://github.com/jessevdk/go-flags        (BSD License)
+
+*   Test only dependencies:
+    └── https://github.com/stretchr/testify/assert  (MIT License)
+        ├── https://github.com/davecgh/go-spew      (ISC License)
+        └── https://github.com/pmezard/go-difflib   (BSD License)
 `
