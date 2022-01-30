5a77eaa3a9d4b3f9056e4d1dccc36d8ebcd660ad
elij <elij@wundrd.net>
2022-01-30 14:21:58 -0800

    update vers gen

--- cmd/go-camo/main_vers_gen.go
+++ cmd/go-camo/main_vers_gen.go
@@ -11,8 +11,12 @@ https://github.com/cactus/go-camo
 Portions of this software utilize third party libraries:
 *   Runtime dependencies:
     ├── github.com/cactus/mlog (MIT license)
-    └── github.com/jessevdk/go-flags (BSD license)
+    ├── github.com/jessevdk/go-flags (BSD license)
+    ├── github.com/prometheus/client_golang (Apache 2.0)
+    ├── github.com/prometheus/common (Apache 2.0)
+    └── golang.org/x/net (BSD license)
 
 *   Test/Build only dependencies:
-    └── gotest.tools/v3/assert (Apache 2.0)
+    ├── github.com/xlab/treeprint (MIT license)
+    └── gotest.tools/v3 (Apache 2.0)
 `
