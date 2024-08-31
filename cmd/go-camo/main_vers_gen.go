3239ceaa9938a17e83b44fa940424a8aa861ce05
elij <elij@wundrd.net>
2024-08-31 11:01:03 -0700

    update printed license info

--- cmd/go-camo/main_vers_gen.go
+++ cmd/go-camo/main_vers_gen.go
@@ -10,13 +10,15 @@ https://github.com/cactus/go-camo
 
 Portions of this software utilize third party libraries:
 *   Runtime dependencies:
-    ├── github.com/cactus/mlog (MIT license)
-    ├── github.com/alecthomas/kong (MIT license)
+    ├── github.com/alecthomas/kong          (MIT license)
+    ├── github.com/cactus/mlog              (MIT license)
     ├── github.com/prometheus/client_golang (Apache 2.0)
-    ├── github.com/prometheus/common (Apache 2.0)
-    └── golang.org/x/net (BSD license)
+    ├── github.com/prometheus/common        (Apache 2.0)
+    ├── github.com/quic-go/quic-go          (MIT license)
+    ├── go.uber.org/automaxprocs            (MIT license)
+    └── golang.org/x/net                    (BSD license)
 
 *   Test/Build only dependencies:
-    ├── github.com/xlab/treeprint (MIT license)
-    └── gotest.tools/v3 (Apache 2.0)
+    ├── github.com/xlab/treeprint           (MIT license)
+    └── gotest.tools/v3                     (Apache 2.0)
 `
