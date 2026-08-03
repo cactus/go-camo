7c5d012cd533fd6e21677eec85df09c849f7bb89
elij <dropwhile@wundrd.net>
2026-08-03 16:33:09 -0700

    update main_vers_go

--- cmd/go-camo/main_vers_gen.go
+++ cmd/go-camo/main_vers_gen.go
@@ -10,15 +10,13 @@ https://github.com/cactus/go-camo
 
 Portions of this software utilize third party libraries:
 *   Runtime dependencies:
-    ├── github.com/alecthomas/kong          (MIT license)
     ├── codeberg.org/dropwhile/mlog         (MIT license)
+    ├── github.com/alecthomas/kong          (MIT license)
     ├── github.com/prometheus/client_golang (Apache 2.0)
-    ├── github.com/prometheus/common        (Apache 2.0)
     ├── github.com/quic-go/quic-go          (MIT license)
     ├── github.com/rdforte/gomaxecs         (MIT license)
     └── golang.org/x/net                    (BSD license)
 
 *   Test/Build only dependencies:
-    ├── github.com/xlab/treeprint           (MIT license)
-    └── gotest.tools/v3                     (Apache 2.0)
+    └── codeberg.org/dropwhile/assert       (MIT license)
 `
